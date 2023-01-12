62834079ce607e8c782562f7a1818c5383c3fc64
Anees Malik <grace.emp02@gmail.com>
2023-01-12 20:47:57 +0500

    bug fixes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://192.168.18.5/red/';
-// axios.defaults.baseURL = 'https://iamredapple.com/php/';
+// axios.defaults.baseURL = 'http://192.168.18.5/red/';
+axios.defaults.baseURL = 'https://iamredapple.com/php/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
